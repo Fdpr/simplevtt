a5e3dead54d36ee4aacebb755e3480cd5496ef49
Florian <70607158+Fdpr@users.noreply.github.com>
2022-03-31 23:11:16 +0200

    save and load skeleton

---
+++ src/filehandle.js
@@ -0,0 +1,19 @@
+// Loads standard Global configurations
+function loadConfig(){
+
+}
+
+// Loads local Configuration and overrides settings if necessary
+function loadLocalConfig(){
+
+}
+
+// Loads whole VTT including tokens and backgrounds
+function loadVTT(){
+
+}
+
+// Saves wholes VTT including tokens and backgrounds
+function saveVTT(){
+
+}
